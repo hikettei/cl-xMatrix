f64b701832749829cf82834184bd11e2ca68ab54
hikettei <hikettei@gmail.com>
2023-04-13 16:26:58 +0900

    add: mithral.dylib

--- source/package.lisp
+++ source/package.lisp
@@ -13,14 +13,19 @@
 (defun get-shared-xmatrix-pathname ()
   (asdf:system-relative-pathname "cl-xmatrix" "./source/kernel/libxMatrix.dylib"))
 
+(defun get-shared-libmaddness-pathname ()
+  (asdf:system-relative-pathname "cl-xmatrix" "./source/kernel/libMithral.dylib"))
+
 (defun xmatrix-built-p ()
   t)
 
 ; Fixme: define-foreign-library and use-foreign-lib...
 (defun load-xmatrix ()
-  (let ((libpath (get-shared-xmatrix-pathname)))
+  (let ((libpath (get-shared-xmatrix-pathname))
+	(libpath1 (get-shared-libmaddness-pathname)))
     ; Add: conditions and handler-case
-    (load-foreign-library libpath)))
+    (load-foreign-library libpath)
+    (load-foreign-library libpath1)))
 
 (defun cpu-information (&key (stream t))
   "Displays cpu-information, TODO: for more details"
